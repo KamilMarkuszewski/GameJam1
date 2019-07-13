95635afca1b34025806fff3b1a714b48a46315ac
KamilMarkuszewski <kajt_sweb@tlen.pl>
2019-07-13 17:07:39 +0200

    shits, human

---
+++ Game/GameJam1/Assets/DestroyAfterSec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class DestroyAfterSec : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
